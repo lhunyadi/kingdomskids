f74a49d487bb8eac83a56f7fa1ec4f5ffec48f9b
Your Name <lhunyadi@outlook.com>
2025-11-13 23:54:30 +0100

    Addition: Inner <div>

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,8 @@
 export default function Header() {
   return (
-    <header>
-
+    <header className="w-full h-15 p-2 bg-blue-600">
+      <div className="w-full h-full bg-black rounded-md">
+      </div>
     </header>
   );
 }
